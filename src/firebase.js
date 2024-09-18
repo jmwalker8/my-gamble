5a9b3c9664810fb44ff3d8ee2ef9bfa50f921caf
jmwalker <walkerjulian888@gmail.com>
2024-09-18 18:13:05 -0500

    Changes

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js';
-import { getAuth } from 'https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js';
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.12.3/firebase-firestore.js';
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDK4bZaMjjctDjdG-8rdDkczPHA8tbMl_0",
@@ -9,11 +9,11 @@ const firebaseConfig = {
   projectId: "my-gamble",
   storageBucket: "my-gamble.appspot.com",
   messagingSenderId: "362532353546",
-  appId: "1:362532353546:web:0cf16960c9a8b61e0311f3",
+  appId: "1:362532353546:web:0cf16960c9a8b61e0311f3"
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-export { auth, app, db };
+export { auth, db };
